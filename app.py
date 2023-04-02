afaf6259d2a1c46f7677ef031e733a03a4eb56c9
Reshma Kamble <116890304+Batgirl21@users.noreply.github.com>
2023-04-02 18:32:18 +0000

    added dynamic data and route

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+PROPERTIES = [
+  {
+    'room_no': 501,
+    'title': '1BHK',
+    'location': 'Dublin, Ireland',
+    'price': '$50000'
+  },
+  {
+    'room_no': 502,
+    'title': '1.5BHK',
+    'location': 'Dublin, Ireland',
+    'price': '$70000'
+  },
+  {
+    'room_no': 605,
+    'title': '3BHK',
+    'location': 'Dublin, Ireland',
+    'price': '$90000'
+  },
+]
 
 
 @app.route("/")
 def hello_world():
-  return "Hello, World!"
+  return render_template('home.html', properties=PROPERTIES)
+
+
+@app.route("/api/properties")
+def list_properies():
+  return jsonify(PROPERTIES)
 
 
 if __name__ == "__main__":
